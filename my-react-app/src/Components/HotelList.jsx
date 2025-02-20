2544dc4afdd7901d032eabe6da3399c3ffc7690a
Gouda12345 <gouda8051@gmail.com>
2025-02-20 21:03:46 +0530

    first commit

--- my-react-app/src/Components/HotelList.jsx
+++ my-react-app/src/Components/HotelList.jsx
@@ -1,37 +1,20 @@
 import React from "react";
 import "./HotelList.css";
 import { IoSearch } from "react-icons/io5";
+import { CiBookmark } from "react-icons/ci";
+import { TiStarOutline } from "react-icons/ti";
+import { IoLocationOutline } from "react-icons/io5"
+import { CiHome } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { TbAlignBoxCenterStretch } from "react-icons/tb";
+import { CiUser } from "react-icons/ci";
 import { FaHotel, FaPlane, FaMapMarkerAlt, FaUtensils } from "react-icons/fa";
 
-const hotels = [
-  // {
-  //   id: 1,
-  //   name: "Hotel Royal Blue",
-  //   location: "Zurich, Switzerland",
-  //   price: "$480/night",
-  //   rating: "4.9 (6.8K)",
-  //   image: "hotel.jpg",
-  // },
-  // {
-  //   id: 2,
-  //   name: "Warwick Grand Hotel",
-  //   location: "New York, USA",
-  //   price: "$480/night",
-  //   rating: "5.0 (2.6K)",
-  //   image: "hotel.jpg",
-  // },
-  // {
-  //   id: 3,
-  //   name: "Water Garden Inn",
-  //   location: "London, UK",
-  //   price: "$420/night",
-  //   rating: "4.8 (5.2K)",
-  //   image: "hotel.jpg",
-  // },
-];
 
 const HotelList = () => {
   return (
+    <div className="explore-hotels">
     <div className="hotel-container">
         <div className="head">
             <h4>Let's </h4>
@@ -70,21 +53,39 @@ const HotelList = () => {
           <span className="dot"></span>
         </div>
       </div>
-        {/* {hotels.map((hotel) => (
-          <div className="hotel-card" key={hotel.id}>
-            <img src={hotel.image} alt={hotel.name} className="hotel-image" />
-            <div className="hotel-info">
-              <h3>{hotel.name}</h3>
-              <p className="hotel-location">{hotel.location}</p>
-              <div className="hotel-details">
-                <span className="price">{hotel.price}</span>
-                <span className="rating">{hotel.rating}</span>
-              </div>
-            </div>
-          </div>
-        ))} */}
+      <div className="bookmarks"><CiBookmark /></div>
+      <div className="hotel-title">Hotel Royal Blue</div>
+      <div className="location"><IoLocationOutline /> Zerich Switzerland</div>
+      <div className="rate">
+        <div className="dollar">$480/<a className="night">night</a></div>
+        <div className="rating"><TiStarOutline /> 4.9<a className="rat">(6.8K)</a></div>
       </div>
     </div>
+    <div className="low-cost">Low Cost Hotels</div>
+    <div className="grand-box">
+      <div className="warwick">
+        <div className="warwick-imgg"><img src="https://th.bing.com/th/id/OIP.iH5GIeX6I5wYHgVsUtu51QHaE7?w=226&h=180&c=7&r=0&o=5&pid=1.7"></img></div>
+        <div className="warwick-title">Warwick Grand Hotel<p className="location-new"><IoLocationOutline /> New York</p>
+        <p className="star"><TiStarOutline /> 5.0<a className="rat1">(2.4K)<a className="dollar">$480/</a><a className="night">night</a></a></p></div>
+      </div>
+    </div>
+    <div className="grand-box2">
+    <div className="warwick">
+        <div className="warwick-imgg"><img src="https://th.bing.com/th/id/OIP.tDK4UMcUN5bkrLn2CqtCeQHaE8?w=216&h=180&c=7&r=0&o=5&pid=1.7"></img></div>
+        <div className="warwick-title1">Warwick Grand int</div></div>
+    </div>
+    <div className="icons-bottom">
+      <div className="icons">
+      <a className="icons1"><CiHome /></a>
+      <a className="icons2"><CiHeart /></a>
+      <a className="icons2"><TbAlignBoxCenterStretch /></a>
+      <a className="icons2"><IoChatboxEllipsesOutline /></a>
+      <a className="icons2"><CiUser /></a>
+      </div>
+    </div>
+  </div>
+  </div>
+  
   );
 };
 
